a2ae1bfda0806317d59b3c119a71c48523bb45ee
bsong1124 <bsong1124@gmail.com>
2023-11-22 15:03:41 -0500

    add show functionality

--- music-playlist-app/routes/playlists.js
+++ music-playlist-app/routes/playlists.js
@@ -1,11 +1,11 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const playlistCtrl = require('../controllers/playlists')
+const playlistCtrl = require("../controllers/playlists");
 
 // GET /playlists
 // router.get('/', playlistCtrl.index)
 
-router.get('/new', playlistCtrl.new)
-
-router.post('/', playlistCtrl.create)
+router.get("/new", playlistCtrl.new);
+router.post("/", playlistCtrl.create);
+router.get("/:id", playlistCtrl.show);
 module.exports = router;
