ff575af577061f8b05a25ef1ac19cbc0c7e9be16
nvandy23 <nvandy2373@gmail.com>
2023-11-22 11:22:36 -0500

    add new and create functions in controller added new and create routes

--- music-playlist-app/controllers/playlists.js
+++ music-playlist-app/controllers/playlists.js
@@ -1,32 +1,33 @@
-const Playlist =require('../models/playlist')
+const Playlist = require("../models/playlist");
 
-
-
-async function create(req,res) {
-    for (let key in req.body) {
-        if (req.body[key] === "") delete req.body[key];
-      }
-    try {
-     Playlist.create(req.body)
-     res.redirect('/playlist')
-    }
-    catch (err){
-        console.log('create error',err)
-        res.render('playlists/new',{errorMsg:err.message })
-    }
+function newPlaylist(req, res) {
+  res.render("playlists/new", { title: "Add Playlists", errorMsg: "" });
 }
 
-async function index(req, res) {
-    try {
-      const restaurants = await Restaurant.find().sort("yearOpened");
-      res.render("restaurants/index", { title: "All Restaurants", restaurants });
-    } catch (err) {
-      console.log("index error", err);
-    }
+async function create(req, res) {
+  for (let key in req.body) {
+    if (req.body[key] === "") delete req.body[key];
   }
-
-
-module.exports ={
-    index,
-    create
+  try {
+    Playlist.create(req.body);
+    res.redirect("/playlist");
+  } catch (err) {
+    console.log("create error", err);
+    res.render("playlists/new", { errorMsg: err.message });
+  }
 }
+
+// async function index(req, res) {
+//     try {
+//       const playlist = await Playlist.find().sort("yearOpened");
+//       res.render("playlists/index", { title: 'all playlist', playlist });
+//     } catch (err) {
+//       console.log("index error", err);
+//     }
+//   }
+
+module.exports = {
+  //index,
+  create,
+  new: newPlaylist,
+};
