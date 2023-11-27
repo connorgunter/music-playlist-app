b684958317d98f186f5cecf5a235cba85686b699
Zach Kurfirst <zachmk11@gmail.com>
2023-11-27 15:04:55 -0500

    configure passport with passport.use(), verify callback, modify user model, serializeUser and deserializeUser methods

--- music-playlist-app/models/user.js
+++ music-playlist-app/models/user.js
@@ -1,10 +1,15 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const userSchema = new Schema({
-  name: String
-}, {
-  timestamps: true
-});
+const userSchema = new Schema(
+  {
+    name: String,
+    googleId: { type: String, required: true },
+    email: String,
+    avatar: String,
+  },
+  {
+    timestamps: true,
+  });
 
-module.exports = mongoose.model('User', userSchema);
+module.exports = mongoose.model("User", userSchema);
