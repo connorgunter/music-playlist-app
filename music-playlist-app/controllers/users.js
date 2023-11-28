ba7e696348f768c7d75eba2ef18d32a1b633fc9b
Connor Gunter <connorgunter@Connors-MacBook-Pro.local>
2023-11-28 09:51:55 -0500

    morning review

---
+++ music-playlist-app/controllers/users.js
@@ -0,0 +1,15 @@
+// const User = require("../models/user");
+
+// async function showAvatar(req, res) {
+//     try{
+//     const user = await User.findById(req.params.id)
+
+//     } catch(err) {
+//         console.log(err)
+//     }
+
+// }
+
+// module.exports = {
+//     showAvatar
+// }
