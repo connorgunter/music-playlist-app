6ba012c02f4c7415c4927386e7dfb7e6a5182d07
Zach Kurfirst <zachmk11@gmail.com>
2023-11-28 11:56:21 -0500

    update playlists route and show view incoming changes

--- routes/playlists.js
+++ routes/playlists.js
@@ -1,19 +1,6 @@
 const express = require("express");
 const router = express.Router();
 const playlistCtrl = require("../controllers/playlists");
-<<<<<<< HEAD
-
-router.get("/", playlistCtrl.myIndex);
-// Remember: new route always bfore show
-router.get("/new", playlistCtrl.new);
-router.get("/:id", playlistCtrl.show);
-// 1. GET /playlists/:id/edit
-router.get("/:id/edit", playlistCtrl.edit);
-router.post("/", playlistCtrl.create);
-router.delete("/:id", playlistCtrl.delete);
-// 2. PUT /playlists/:id
-router.put('/:id', playlistCtrl.update)
-=======
 const ensureLoggedIn = require("../config/ensureLoggedIn");
 
 router.get("/", ensureLoggedIn, playlistCtrl.myIndex);
@@ -26,6 +13,5 @@ router.post("/", playlistCtrl.create);
 router.delete("/:id", ensureLoggedIn, playlistCtrl.delete);
 // 2. PUT /playlists/:id
 router.put("/:id", playlistCtrl.update);
->>>>>>> development
 
 module.exports = router;
