3efc7cfaa455084b06e6e58fc555bd0fb5c6801d
Zach Kurfirst <zachmk11@gmail.com>
2023-11-28 22:27:10 -0500

    work on search functionality

--- routes/api.js
+++ routes/api.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const router = express.Router();
-const passport = require('passport');
+const passport = require("passport");
 const songsCtrl = require("../controllers/api");
 const ensureLoggedIn = require("../config/ensureLoggedIn");
 
-router.get('/playlists/:id/search', ensureLoggedIn,songsCtrl.add)
+// start from http://localhost:3000/
 
+// GET search page
+router.get("/search", ensureLoggedIn, songsCtrl.add);
+// GET search results
+router.get("/search", songsCtrl.search);
 
-
-module.exports =router
+module.exports = router;
