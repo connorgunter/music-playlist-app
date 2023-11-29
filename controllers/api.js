42ed23d45b34e2394c7e60b7fb1b8084ddd7691a
nvandy23 <nvandy2373@gmail.com>
2023-11-28 19:53:09 -0500

    add function in controller and add search to search template

--- controllers/api.js
+++ controllers/api.js
@@ -1,9 +1,25 @@
 
 const Song = require("../models/api");
+const fetch =require('node-fetch')
 
+function add(req, res) {
+    req.body.user = req.user._id;
+    req.body.userName = req.user.name;
+    req.body.userAvatar = req.user.avatar;
+    res.render("playlists/search", { title: "Search a song", errorMsg: "" });
+  }
 
+async function search (req,res) {
 
+  }
+
+async function addToPlaylist(req, res) {
+
+  }
 
 module.exports = {
+add,
+search,
+addToPlaylist
  
   };
--- routes/api.js
+++ routes/api.js
@@ -2,3 +2,10 @@ const express = require("express");
 const router = express.Router();
 const passport = require('passport');
 const songsCtrl = require("../controllers/api");
+const ensureLoggedIn = require("../config/ensureLoggedIn");
+
+router.get('/playlists/:id/search', ensureLoggedIn,songsCtrl.search)
+
+
+
+module.exports =router
