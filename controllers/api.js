1dd46d54897dcda25b24289d38874efeddcb3ffa
Connor Gunter <connorgunter@Connors-MacBook-Pro.local>
2023-11-29 13:42:58 -0500

    add logo to top-page

--- controllers/api.js
+++ controllers/api.js
@@ -2,7 +2,7 @@ const Song = require("../models/api");
 // const fetch = require("node-fetch");
 const Playlist = require("../models/playlist");
 const token = process.env.LASTFM_TOKEN;
-const ROOT_URL = "https://ws.audioscrobbler.com/2.0"
+const ROOT_URL = "https://ws.audioscrobbler.com/2.0";
 
 function add(req, res) {
   req.body.user = req.user._id;
@@ -13,29 +13,26 @@ function add(req, res) {
     title: "Search a Song",
     errorMsg: "",
     playlist,
-    queryData: null
+    queryData: null,
   });
 }
 
 function search(req, res, next) {
   const q = req.query.q;
   // const results = queryData.results.trackmatches.track
-  if (!q) return res.render('songs/search', { queryData: null })
-  fetch(`${ROOT_URL}/?method=track.search&track=${q}&api_key=${token}&format=json&limit=10`)
-  .then(res => res.json())
-  .then(queryData => {
-    res.render("songs/search", {
-      title: `Search Results: ${q}`,
-      errorMsg: "",
-      queryData
+  if (!q) return res.render("songs/search", { queryData: null });
+  fetch(
+    `${ROOT_URL}/?method=track.search&track=${q}&api_key=${token}&format=json&limit=10`
+  )
+    .then((res) => res.json())
+    .then((queryData) => {
+      res.render("songs/search", {
+        title: `Search Results: ${q}`,
+        errorMsg: "",
+        queryData,
+      });
     });
-  })
-
 }
-
-
-
-
 
 // async function addToPlaylist(req, res) {}
 
