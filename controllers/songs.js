66b26f75dfb1f4182db359ecf167828093d11459
nvandy23 <nvandy2373@gmail.com>
2023-11-30 15:30:24 -0500

    push songs and artist into playlist schema

--- controllers/songs.js
+++ controllers/songs.js
@@ -17,7 +17,7 @@ async function newSongs(req, res) {
   });
 }
 
-async function search(req, res,queryData) {
+async function search(req, res, queryData) {
   const q = req.query.q;
   const playlist = await Playlist.findById(req.params.id);
   try {
@@ -27,18 +27,18 @@ async function search(req, res,queryData) {
     )
       .then((res) => res.json())
       .then((queryData) => {
-        const results = queryData.results.trackmatches.track.map(r => { 
-          r.safe_name = r.name.replace(/\?/g,'%3F')
-          console.log(r.safe_name)
-          return r
-        } )
-        console.log('queryData:', results[1])
+        const results = queryData.results.trackmatches.track.map((r) => {
+          r.safe_name = r.name.replace(/\?/g, "%3F");
+          //console.log(r.safe_name)
+          return r;
+        });
+        console.log("queryData:", results[1]);
         res.render("songs/search", {
           title: `Search Results: ${q}`,
           errorMsg: "",
           queryData,
           playlist,
-          results
+          results,
         });
       });
   } catch (err) {
@@ -48,27 +48,41 @@ async function search(req, res,queryData) {
 
 // triggered by clicking of + button
 async function addToPlaylist(req, res) {
+  console.log("line 46:", req.params);
   const playlist = await Playlist.findById(req.params.id);
-  // console.log('line 46:', req.params)
-  const {name, artist} = req.params
+  const { name, artist } = req.params;
   // const encodeName = encodeURIComponent(name).replace(/\?/g,'%3F')
   // console.log('line 49:', encodeName)
   try {
     fetch(
-        `${ROOT_URL}/?method=track.getInfo&api_key=${token}&artist=${artist}&track=${name}&format=json`)
+      `${ROOT_URL}/?method=track.getInfo&api_key=${token}&artist=${artist}&track=${name}&format=json`
+    )
       .then((res) => res.json())
       .then((songData) => {
-        // console.log('line 52:', encodeURIComponent(songData.track.name).replace(/\?/g,'%3F'))
-        res.send(`fetch song data ${songData.track.name}:${songData.track.artist.name}`)
+        playlist.songs
+          .push({
+            name: songData.track.name,
+            artist: songData.track.artist.name,
+          })
+          playlist.save()
         });
+          //await playlist.save();
+          console.log(playlist.songs)
+          console.log(playlist)
+        res.redirect(`/playlists/${playlist._id}/search`);
       
-   //const {name,artist,url} =req.body
-   //console.log('this is req.body',req.body)
-  // playlist.songs.push({name,artist,url})
-   //await playlist.save()
-   //console.log(playlist)
-   
-    //res.send(`add song to playlist id: ${req.params.id}`);
+        // console.log('line 52:', encodeURIComponent(songData.track.name).replace(/\?/g,'%3F'))
+        //res.render(`fetch song data ${songData.track.name}:${songData.track.artist.name}
+        //)
+        //   });
+
+        //const {name,artist,url} =req.body
+        //console.log('this is req.body',req.body)
+        // playlist.songs.push({name,artist,url})
+
+        //console.log(playlist)
+
+        //res.send(`add song to playlist id: ${req.params.id}`);
   } catch (err) {
     console.log(err);
   }
