8ff4b63cd4854a59c9c33e36fa4d8be377a28a7b
Zach Kurfirst <zachmk11@gmail.com>
2023-11-30 13:01:17 -0500

    complete successful retrieval of song data from addToPlaylist function; replace ? characters with encoded variable

--- server.js
+++ server.js
@@ -45,9 +45,9 @@ app.use("/playlists", playlistsRouter);
 app.use("/playlists", songsRouter);
 
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  next(createError(404));
-});
+// app.use(function (req, res, next) {
+//   next(createError(404));
+// });
 
 // error handler
 app.use(function (err, req, res, next) {
